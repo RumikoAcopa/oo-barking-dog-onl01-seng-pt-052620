5f4962abbf4c2f1f7c286c508281b1cc522e33ff
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-10 01:08:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,18 @@
 class Dog
   def name=(dog_name)
-    this_dogs_name = dog_name
+    @this_dogs_name = dog_name
   end
 
-  end
+      def name 
+        @this_dogs_name
+      end
+
     def bark
       puts "woof!"
     end
 end
+
+fido = Dog.new
+fido.name = "Fido"
+
+puts fido.name
